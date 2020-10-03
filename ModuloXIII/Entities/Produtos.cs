57e3e85339149adf06f5e5ed7f8783f1338523da
Luiz Henrique <comluizbjj@gmail.com>
2020-10-03 01:30:38 -0300

    Exercicio para tratamento de arquivos.

---
+++ ModuloXIII/Entities/Produtos.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloXIII.Entities
+{
+    class Produtos
+    {
+        public string NameProduto { get; set; }
+        public double Valor { get; set; }
+
+        public int Qtd { get; set; }
+
+        public Produtos() { }
+        public Produtos(string nameProduto, double valor, int qtd)
+        {
+            NameProduto = nameProduto;
+            Valor = valor;
+            Qtd = qtd;
+        }
+
+        public double CalcEstoque()
+        {
+            return Valor * Qtd;
+        }
+    }
+}
---
+++ ModuloXIII/Program.cs
@@ -0,0 +1,49 @@
+﻿using ModuloXIII.Entities;
+using System;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+
+namespace ModuloXIII
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path  = @"C:\Users\Luiz Henrique\source\repos\ConsoleApp1\ModuloXIII\Arquivo\dados.csv";
+            string path2 = @"C:\Users\Luiz Henrique\source\repos\ConsoleApp1\ModuloXIII\Arquivo\Out\summary.csv";
+
+            try
+            {
+                
+                string[] lines = File.ReadAllLines(path);
+                using (StreamWriter sw = File.CreateText(path2))
+                    
+                {
+                    foreach (String line in lines)
+                    {
+                        string[] fields = line.Split(',');
+
+                        Produtos dados = new Produtos();
+
+                        dados.NameProduto = fields[0];
+                        dados.Valor = double.Parse(fields[1], CultureInfo.InvariantCulture);
+                        dados.Qtd = int.Parse(fields[2]);
+
+                        double total = dados.CalcEstoque();
+
+                        string dadosImpressos = dados.NameProduto+";"+ total.ToString("F2", CultureInfo.InvariantCulture);
+                        sw.WriteLine(dadosImpressos);
+
+                    }
+                }
+                
+            }
+            catch (IOException erro)
+            {
+                Console.WriteLine("UM ERRO OCORRIDO: ");
+                Console.WriteLine(erro.Message);
+            }
+        }
+    }
+}
