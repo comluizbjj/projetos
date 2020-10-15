bfe9eccfb05a977c332a9890b7c89746cce0765e
Luiz Henrique <comluizbjj@gmail.com>
2020-10-15 00:01:58 -0300

    Exercicio modulo XV com colecoes Hash

---
+++ ModuloXV/Entities/Students.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ModuloXV.Entities
+{
+    class Students
+    {
+        public int IdStudents { get; set; }
+        public string Course { get; set; }
+
+       
+
+        public override int GetHashCode()
+        {
+            return IdStudents.GetHashCode();
+        }
+
+        public override bool Equals(object obj)
+        {
+            if(!(obj is Students))
+            {
+                return false;
+            }
+            Students other = obj as Students;
+            return IdStudents.Equals(other.IdStudents);
+        }
+    }
+}
---
+++ ModuloXV/Program.cs
@@ -0,0 +1,33 @@
+﻿using ModuloXV.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ModuloXV
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            HashSet<Students> set = new HashSet<Students>();
+
+            String[] course = new string[3];
+            course[0] = "A";
+            course[1] = "B";
+            course[2] = "C";
+
+            for(int cont=0;cont < course.Length;cont++)
+            {
+                Console.Write("How many students for course " + course[cont] + "? ");
+                int qtdStudents = int.Parse(Console.ReadLine());
+
+                for (int cont2 = 0; cont2 < qtdStudents; cont2++)
+                {
+                    int matricula = int.Parse(Console.ReadLine());
+                    set.Add(new Students { IdStudents = matricula, Course = course[cont]});
+                }
+            }
+            Console.WriteLine("Total students: " + set.Count);
+        }
+    }
+}
